a0d140ce9e8b598a9a413972f3692b046b95e448
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2021-10-20 13:05:26 -0300

    ex
    03

---
+++ exercicios/ex0003.py
@@ -0,0 +1,10 @@
+# / tipos primitivos (
+# /int = 4,-4,0,9875
+# /float = 4.5 , 0.0076 , -15.223 , 7.0
+# /bool = True , False -
+# /str = "olá" , "7.5" , " "
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+soma = n1 + n2
+print("A soma vale {} ".format(soma))
+
